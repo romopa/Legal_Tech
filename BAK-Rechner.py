332dfe561df88f1bf6bc7ec0f239a5244099616a
romopa <82755755+romopa@users.noreply.github.com>
2021-04-20 18:02:56 +0200

    Update BAK-Rechner.py

--- BAK-Rechner.py
+++ BAK-Rechner.py
@@ -1,9 +1,9 @@
 # Programm 1: BAK-Rechner
 # (c) M Rowold 2021-04-18
 
-menge_alkohol_liter = float(input("Wieviele Liter Alkohol wurden getrunken? (in Liter) "))
+menge_alkohol_liter = float(input("Wieviele Liter Alkohol wurden getrunken? (in Liter, z.B. 1.5) "))
 alkohol_anteil = float(input("Alkoholanteil in Prozent (z.B. 0.12): "))
-gewicht_der_person =int(input("Wieviel wiegt die Person? (in kg) "))
+gewicht_der_person =float(input("Wieviel wiegt die Person? (in kg) "))
 masse_alkohol = menge_alkohol_liter *alkohol_anteil*0.8
 geschlecht = input("Ist die getestete Person ein Mann oder eine Frau? (M oder F) ")
 if geschlecht == "M":
